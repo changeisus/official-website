1b6420b7c808b56262a97d527d6acc66c6511062
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-12-31 10:49:52 +0530

    added blogs from js

---
+++ js/blog_data.js
@@ -0,0 +1,34 @@
+var blog_data=[
+    {
+        "title":"Hurricane Irma has devastated Florida1",
+        "author":"Hayden Cordeiro1",
+        "date":"Sept 10, 20181",
+        "imageURL":"images/image_2.jpg",
+        "content":"A small river named Duden flows by their place and supplies it with the necessary regelialia.1",
+        "hyperlink":"https://www.google.com/"
+    },
+    {
+        "title":"Hurricane Irma has devastated Florida2",
+        "author":"Hayden Cordeiro2",
+        "date":"Sept 10, 2018",
+        "imageURL":"images/image_2.jpg",
+        "content":"A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+        "hyperlink":"https://www.google.com/"
+    },
+    {
+        "title":"Hurricane Irma has devastated Florida3",
+        "author":"Hayden Cordeiro",
+        "date":"Sept 10, 2018",
+        "imageURL":"images/image_2.jpg",
+        "content":"A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+        "hyperlink":"https://www.google.com/"
+    },
+    {
+        "title":"Hurricane Irma has devastated Florida4",
+        "author":"Hayden Cordeiro",
+        "date":"Sept 10, 2018",
+        "imageURL":"images/image_2.jpg",
+        "content":"A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+        "hyperlink":"https://www.google.com/"
+    }
+]

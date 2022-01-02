77acd812eeedba90daf8fe54e5da8febe20ba9c0
hisenberggg <hknocks8@gmail.com>
2022-01-02 22:48:29 +0530

    blogs and about

--- js/blog_data.js
+++ js/blog_data.js
@@ -1,34 +1,82 @@
 var blog_data=[
     {
-        "title":"Hurricane Irma has devastated Florida1",
-        "author":"Hayden Cordeiro1",
-        "date":"Sept 10, 20181",
-        "imageURL":"images/image_2.jpg",
-        "content":"A small river named Duden flows by their place and supplies it with the necessary regelialia.1",
-        "hyperlink":"https://www.google.com/"
+        "title":"Fast Fashion: An Overhyped Trend",
+        "author":"Arham Shah",
+        "date":"September 29, 2021",
+        "imageURL":"https://1.bp.blogspot.com/-oU2SWR2cJCk/YVQmOfCeEKI/AAAAAAAAAPo/YrbzmNmny88hDv89W2x1Is4cjkm2lHYuACLcBGAsYHQ/s320/1234567.jpg",
+        "content":"The constant change in fashion trends’ affect on the environment.",
+        "hyperlink":"https://changeisus11.blogspot.com/2021/09/fast-fashion.html"
     },
     {
-        "title":"Hurricane Irma has devastated Florida2",
-        "author":"Hayden Cordeiro2",
-        "date":"Sept 10, 2018",
-        "imageURL":"images/image_2.jpg",
-        "content":"A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-        "hyperlink":"https://www.google.com/"
+        "title":"Climate Change: Myth of Fact?",
+        "author":"Arham Shah",
+        "date":"October 23, 2021",
+        "imageURL":"https://lh3.googleusercontent.com/-Ya4vpmbcNU4/YXPsdtJthcI/AAAAAAAAAQk/Mb-PlQaBElYKr76PgBBOCSv9-soWWt3HwCLcBGAsYHQ/w499-h279/image.png",
+        "content":"The harmful changes on Earth with proven facts.",
+        "hyperlink":"https://changeisus11.blogspot.com/2021/10/climate-change-myth-or-fact.html"
     },
     {
-        "title":"Hurricane Irma has devastated Florida3",
-        "author":"Hayden Cordeiro",
-        "date":"Sept 10, 2018",
-        "imageURL":"images/image_2.jpg",
-        "content":"A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-        "hyperlink":"https://www.google.com/"
+        "title":"Human Activities Affecting the Environment",
+        "author":"Arham Shah",
+        "date":"November 01, 2021",
+        "imageURL":"https://lh3.googleusercontent.com/-EKVbVNUmYVc/YX-22wsSXiI/AAAAAAAAATE/qSlYCajR8tEn2YiyydK6MGKp3kAJFKWLgCLcBGAsYHQ/w320-h181/image.png",
+        "content":"What are the harmful impacts of human activities on Earth?",
+        "hyperlink":"https://changeisus11.blogspot.com/2021/11/activities-affecting-environment-age-of.html"
     },
     {
-        "title":"Hurricane Irma has devastated Florida4",
-        "author":"Hayden Cordeiro",
-        "date":"Sept 10, 2018",
-        "imageURL":"images/image_2.jpg",
-        "content":"A small river named Duden flows by their place and supplies it with the necessary regelialia.",
-        "hyperlink":"https://www.google.com/"
-    }
+        "title":"Decline of Natural Resources",
+        "author":"Arham Shah",
+        "date":"November 11, 2021",
+        "imageURL":"https://lh3.googleusercontent.com/-5iURCIuk0QY/YY32qBcD6NI/AAAAAAAAATk/dEoL7QXjE5cXBpDw0V4au8ST56QSKkITgCLcBGAsYHQ/image.png",
+        "content":"How are natural resources depleting at such an alarming rate?",
+        "hyperlink":"https://changeisus11.blogspot.com/2021/11/decline-of-natural-resources.html"
+    },
+    {
+        "title":"Combating the Rising Tide of Plastic Pollution: Beach Cleanups",
+        "author":"Arham Shah",
+        "date":"January 01, 2022",
+        "imageURL":"https://lh3.googleusercontent.com/-5iURCIuk0QY/YY32qBcD6NI/AAAAAAAAATk/dEoL7QXjE5cXBpDw0V4au8ST56QSKkITgCLcBGAsYHQ/image.png",
+        "content":"How do beach cleanups help to battle plastic pollution?",
+        "hyperlink":"https://changeisus11.blogspot.com/2022/01/combating-rising-tide-of-plastic.html "
+    },
+    {
+        "title":"Carbon Footprints!",
+        "author":"Arham Shah",
+        "date":"January 01, 2022",
+        "imageURL":"https://lh3.googleusercontent.com/-5iURCIuk0QY/YY32qBcD6NI/AAAAAAAAATk/dEoL7QXjE5cXBpDw0V4au8ST56QSKkITgCLcBGAsYHQ/image.png",
+        "content":"The impact of your daily life on the environment.",
+        "hyperlink":"https://changeisus11.blogspot.com/2022/01/carbon-footprints.html"
+    },
+    {
+        "title":"Is Plastic Really As Bad As People Say?",
+        "author":"Arham Shah",
+        "date":"January 01, 2022",
+        "imageURL":"https://lh3.googleusercontent.com/-5iURCIuk0QY/YY32qBcD6NI/AAAAAAAAATk/dEoL7QXjE5cXBpDw0V4au8ST56QSKkITgCLcBGAsYHQ/image.png",
+        "content":"Plastic negatively impacts everything we do. How can we curb its use?",
+        "hyperlink":"https://changeisus11.blogspot.com/2022/01/is-plastic-really-as-bad-as-people-say.html "
+    },
+    {
+        "title":"Upcycling Your Clothes",
+        "author":"Arham Shah",
+        "date":"January 01, 2022",
+        "imageURL":"https://lh3.googleusercontent.com/-5iURCIuk0QY/YY32qBcD6NI/AAAAAAAAATk/dEoL7QXjE5cXBpDw0V4au8ST56QSKkITgCLcBGAsYHQ/image.png",
+        "content":"Recycling your clothes and items with your own twist!",
+        "hyperlink":"https://changeisus11.blogspot.com/2022/01/upcycling-your-clothes.html "
+    },
+    {
+        "title":"The Quest for Survival in 2050",
+        "author":"Arham Shah",
+        "date":"January 01, 2022",
+        "imageURL":"https://lh3.googleusercontent.com/-5iURCIuk0QY/YY32qBcD6NI/AAAAAAAAATk/dEoL7QXjE5cXBpDw0V4au8ST56QSKkITgCLcBGAsYHQ/image.png",
+        "content":"What is 2050 really going to be like?",
+        "hyperlink":"https://changeisus11.blogspot.com/2022/01/the-quest-for-survival-in-2050.html "
+    },
+    {
+        "title":"Staying Close to Nature",
+        "author":"Arham Shah",
+        "date":"January 01, 2022",
+        "imageURL":"https://lh3.googleusercontent.com/-5iURCIuk0QY/YY32qBcD6NI/AAAAAAAAATk/dEoL7QXjE5cXBpDw0V4au8ST56QSKkITgCLcBGAsYHQ/image.png",
+        "content":"What can staying close to nature do to you?",
+        "hyperlink":"https://changeisus11.blogspot.com/2022/01/staying-close-to-nature.html "
+    },
 ]

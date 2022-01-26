4f0bb9f2c73658e80dde7fdaa78e245001b17dec
Hayden Cordeiro <cordeirohayden@gmail.com>
2022-01-26 22:32:00 +0530

    updated gallery and blogs

--- create_gallery.py
+++ create_gallery.py
@@ -141,7 +141,7 @@ entire_page='''
         <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
           <div class="col-md-7 ftco-animate text-center" data-scrollax=" properties: { translateY: '70%' }">
              <p class="breadcrumbs" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }"><span class="mr-2"><a href="index.html">Home</a></span> <span>Gallery</span></p>
-            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Galleries</h1>
+            <h1 class="mb-3 bread" data-scrollax="properties: { translateY: '30%', opacity: 1.6 }">Gallery</h1>
           </div>
         </div>
       </div>
